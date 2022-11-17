5c450c573114deeb1b651db97b47b96dc1b040e6
jmiko4 <jmiko4@byui.edu>
2022-11-17 11:36:14 -0700

    added comments

--- Game/Scripting/Clock.cs
+++ Game/Scripting/Clock.cs
@@ -4,14 +4,14 @@ using Unit05.Game.Services;
 namespace Unit05.Game.Scripting{
 
     public class Clock : Action{
-        //Is a clock that keeps track of the game's countdown
+        //Is a clock that keeps track of the number of ticks and increases the length of the Snakes
         private int _tickCount;
         public Clock(){
             _tickCount = 0;
         }
 
         public void Tick(Cast cast){
-            //every 12 ticks is a second of real time
+            //every 5 ticks increases the length of the snakes
             Snake snake = (Snake)cast.GetFirstActor("snake");
             Snake2 snake2 = (Snake2)cast.GetFirstActor("snake2");
             
